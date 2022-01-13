5e931b41f133b053fe7272b7f0a4f0f15e661517
Chebihi <chebihi.infinitysoft@google.com>
2022-01-13 17:56:55 +0000

    Initial commit

---
+++ src/main/java/org/suptech/tp3/entities/Person.java
@@ -0,0 +1,12 @@
+package org.suptech.tp3.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Person {
+    private Long id ;
+    private String name ;
+    private String email;
+}
